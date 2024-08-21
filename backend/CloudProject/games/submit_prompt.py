d07d0708466df04d31df745d92cf0ee4ef7a1cbd
JakeMalmrose <jmalmrose@student.neumont.edu>
2024-08-21 11:01:36 -0600

    Changed wording since we're not using prompt id's

--- backend/CloudProject/games/submit_prompt.py
+++ backend/CloudProject/games/submit_prompt.py
@@ -7,13 +7,13 @@ games_table = dynamodb.Table('Games')
 def lambda_handler(event, context):
     game_id = event['pathParameters']['gameId']
     body = json.loads(event['body'])
-    prompt_id = body['prompt_id']
+    prompt_text = body['prompt_text']
     
     # Update the game with the selected prompt
     games_table.update_item(
         Key={'id': game_id},
         UpdateExpression='SET current_prompt = :prompt',
-        ExpressionAttributeValues={':prompt': prompt_id}
+        ExpressionAttributeValues={':prompt': prompt_text}
     )
     
     return {
